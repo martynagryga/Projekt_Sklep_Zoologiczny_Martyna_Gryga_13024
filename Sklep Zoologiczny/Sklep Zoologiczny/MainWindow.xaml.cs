2e3f7065e65c7417fbe65fc902e0a0e8841a130f
martynagryga <martyna.gryga@microsoft.wsei.edu.pl>
2022-06-07 21:54:07 +0200

    Ribbon Control, Button, Glyph

--- Sklep Zoologiczny/Sklep Zoologiczny/MainWindow.xaml.cs
+++ Sklep Zoologiczny/Sklep Zoologiczny/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Sklep_Zoologiczny
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : ThemedWindow
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
